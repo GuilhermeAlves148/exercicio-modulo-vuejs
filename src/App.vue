28aafe45857bba744d6a019bf805f8bff7cd4390
Guilherme <alvesguilherme148117@gmail.com>
2024-05-02 10:12:16 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <header class="p-5 mt-4 mb-4">
+      <h1>Calculadora aritmética</h1>
+    </header>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
